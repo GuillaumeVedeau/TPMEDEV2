0b62eb99404135a2e1a165b6795c51499df7bbcf
vvielzeu <vvielzeu@pc-b13-13.eleves.ecn>
2014-12-01 11:20:56 +0100

    enleve un commentaire

--- MEDEVTP2/src/medevtp2/MEDEVTP2.java
+++ MEDEVTP2/src/medevtp2/MEDEVTP2.java
@@ -6,7 +6,7 @@ package medevtp2;
 
 /**
  *
- * @author gvedeau
+ * @author vvielzeu
  */
 public class MEDEVTP2 {
 
@@ -14,7 +14,6 @@ public class MEDEVTP2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    // I am a new comment !
 }
