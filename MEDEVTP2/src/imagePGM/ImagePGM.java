9eac311d40360ccdfd57c1579d22b57e2cd66205
gvedeau <gvedeau@pc-b13-12.eleves.ecn>
2014-12-01 11:28:26 +0100

    début de ImagePGM

---
+++ MEDEVTP2/src/imagePGM/ImagePGM.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imagePGM;
+
+/**
+ *gere les objets PGM
+ * @author gvedeau
+ */
+public class ImagePGM {
+    
+    private int hauteur;
+    private int longueur;
+
+    /**
+     * Get the value of longueur
+     *
+     * @return the value of longueur
+     */
+    public int getLongueur() {
+        return longueur;
+    }
+
+    /**
+     * Set the value of longueur
+     *
+     * @param longueur new value of longueur
+     */
+    public void setLongueur(int longueur) {
+        this.longueur = longueur;
+    }
+
+    /**
+     * Get the value of hauteur
+     *
+     * @return the value of hauteur
+     */
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    /**
+     * Set the value of hauteur
+     *
+     * @param hauteur new value of hauteur
+     */
+    public void setHauteur(int hauteur) {
+        this.hauteur = hauteur;
+    }
+
+}
