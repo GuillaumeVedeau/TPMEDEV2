07c27de8fadd25d0652e629825f1383fa54b1c31
gvedeau <gvedeau@pc-b13-12.eleves.ecn>
2014-12-04 12:04:21 +0100

    temporaire

--- MEDEVTP2/test/imagePGM/ImagePGMTest.java
+++ MEDEVTP2/test/imagePGM/ImagePGMTest.java
@@ -34,12 +34,32 @@ public class ImagePGMTest {
     public void testAgrandir() {
         System.out.println("agrandir");
         int rapport = 2;
-        ImagePGM instance = new ImagePGM();
-        ImagePGM expResult = null;
+        ImagePGM instance = new ImagePGM(2,2);
+        instance.getImage().set(0, new Integer(0));
+        instance.getImage().set(1, new Integer(100));
+        instance.getImage().set(2, new Integer(200));
+        instance.getImage().set(3, new Integer(255));
+        
         ImagePGM result = instance.agrandir(rapport);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        assertTrue(result.getImage().get(0).intValue() == instance.getImage().get(0).intValue());
+        assertTrue(result.getImage().get(1).intValue() == instance.getImage().get(0).intValue());
+        assertTrue(result.getImage().get(2).intValue() == instance.getImage().get(1).intValue());
+        assertTrue(result.getImage().get(3).intValue() == instance.getImage().get(1).intValue());
+        assertTrue(result.getImage().get(4).intValue() == instance.getImage().get(0).intValue());
+        assertTrue(result.getImage().get(5).intValue() == instance.getImage().get(0).intValue());
+        assertTrue(result.getImage().get(6).intValue() == instance.getImage().get(1).intValue());
+        assertTrue(result.getImage().get(7).intValue() == instance.getImage().get(1).intValue());
+        assertTrue(result.getImage().get(8).intValue() == instance.getImage().get(2).intValue());
+        assertTrue(result.getImage().get(9).intValue() == instance.getImage().get(2).intValue());
+        assertTrue(result.getImage().get(10).intValue() == instance.getImage().get(3).intValue());
+        assertTrue(result.getImage().get(11).intValue() == instance.getImage().get(3).intValue());
+        assertTrue(result.getImage().get(12).intValue() == instance.getImage().get(2).intValue());
+        assertTrue(result.getImage().get(13).intValue() == instance.getImage().get(2).intValue());
+        assertTrue(result.getImage().get(14).intValue() == instance.getImage().get(3).intValue());
+        assertTrue(result.getImage().get(15).intValue() == instance.getImage().get(3).intValue());
+        
+        
     }
 
     /**
@@ -48,13 +68,32 @@ public class ImagePGMTest {
     @Test
     public void testRetrecir() {
         System.out.println("retrecir");
-        int rapport = 0;
-        ImagePGM instance = new ImagePGM();
-        ImagePGM expResult = null;
+        int rapport = 2;
+        ImagePGM instance = new ImagePGM(4,4);
+        instance.getImage().set(0, new Integer(0));
+        instance.getImage().set(1, new Integer(0));
+        instance.getImage().set(2, new Integer(100));
+        instance.getImage().set(3, new Integer(100));
+        instance.getImage().set(0, new Integer(0));
+        instance.getImage().set(1, new Integer(0));
+        instance.getImage().set(2, new Integer(100));
+        instance.getImage().set(3, new Integer(100));
+        instance.getImage().set(0, new Integer(50));
+        instance.getImage().set(1, new Integer(100));
+        instance.getImage().set(2, new Integer(255));
+        instance.getImage().set(3, new Integer(255));
+        instance.getImage().set(0, new Integer(100));
+        instance.getImage().set(1, new Integer(150));
+        instance.getImage().set(2, new Integer(255));
+        instance.getImage().set(3, new Integer(255));
+        
         ImagePGM result = instance.retrecir(rapport);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        assertTrue(result.getImage().get(0).intValue() == 0);
+        assertTrue(result.getImage().get(1).intValue() == 100);
+        assertTrue(result.getImage().get(2).intValue() == 100);
+        assertTrue(result.getImage().get(3).intValue() == 255);
+        
     }
 
     /**
