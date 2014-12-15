7a1c1053a0d80b321f1e8e1a8745b3387e397cb3
gvedeau <gvedeau@pc-b13-12.eleves.ecn>
2014-12-15 11:22:11 +0100

    tous les test de ImagePGM sont concluants

--- MEDEVTP2/test/imagePGM/ImagePGMTest.java
+++ MEDEVTP2/test/imagePGM/ImagePGMTest.java
@@ -103,31 +103,26 @@ public class ImagePGMTest {
     public void testSeuillage() {
         System.out.println("seuillage");
         
-        ImagePGM testImage = new ImagePGM(2,3);
-        ArrayList<Integer> testPixels = new ArrayList<Integer>();
-        testPixels.add(255);
-        testPixels.add(250);
-        testPixels.add(155);
-        testPixels.add(150);
-        testPixels.add(5);
-        testPixels.add(0);
-        testImage.setImage(testPixels);
-        ImagePGM testImageSeuil = new ImagePGM(2,3);
-        testImageSeuil = testImage.seuillage(152);
+        ImagePGM testImage = new ImagePGM(2,3); 
+        testImage.getImage().set(0, Integer.valueOf(255));
+        testImage.getImage().set(1, Integer.valueOf(250));
+        testImage.getImage().set(2, Integer.valueOf(155));
+        testImage.getImage().set(3, Integer.valueOf(150));
+        testImage.getImage().set(4, Integer.valueOf(5));
+        testImage.getImage().set(5, Integer.valueOf(0));
         
-        ArrayList<Integer> expResult = new ArrayList<>();
-        expResult.add(255);
-        expResult.add(255);
-        expResult.add(255);
-        expResult.add(0);
-        expResult.add(0);
-        expResult.add(0);
+        ImagePGM instance = testImage.seuillage(152);
         
-        assertEquals(expResult, testImageSeuil.getImage());
-        assertEquals(2, testImageSeuil.getHauteur());
-        assertEquals(3, testImageSeuil.getLongueur());
-       
-        assertEquals(null, testImage.seuillage(300));
+        ImagePGM instance2 =  new ImagePGM(2,3);
+        instance2.getImage().set(0, Integer.valueOf(255));
+        instance2.getImage().set(1, Integer.valueOf(255));
+        instance2.getImage().set(2, Integer.valueOf(255));
+        instance2.getImage().set(3, Integer.valueOf(0));
+        instance2.getImage().set(4, Integer.valueOf(0));
+        instance2.getImage().set(5, Integer.valueOf(0));
+        
+        
+        assertEquals(instance2.getImage(), instance.getImage());
         
     }
 
@@ -138,37 +133,37 @@ public class ImagePGMTest {
     public void testDifference() {
         System.out.println("difference");
         
-        ImagePGM testImage = new ImagePGM(2,3);
-        ArrayList<Integer> testPixels = new ArrayList<>();
-        testPixels.add(255);
-        testPixels.add(250);
-        testPixels.add(155);
-        testPixels.add(150);
-        testPixels.add(5);
-        testPixels.add(0);
-        testImage.setImage(testPixels);
-        ImagePGM testImageASoustraire = new ImagePGM(2,3);
-        testPixels.set(0,0);
-        testPixels.set(1,5);
-        testPixels.set(2,150);
-        testPixels.set(3,-200);
-        testPixels.set(4,250);
-        testPixels.set(5,255);
-        testImageASoustraire.setImage(testPixels);
-        ImagePGM result = testImage.difference(testImageASoustraire);
         
-        ArrayList<Integer> expResult = new ArrayList<>();
-        expResult.add(255);
-        expResult.add(245);
-        expResult.add(5);
-        expResult.add(255);
-        expResult.add(0);
-        expResult.add(0);
+        ImagePGM instance = new ImagePGM(4,4);
+        instance.getImage().set(0, new Integer(0));
+        instance.getImage().set(1, new Integer(255));
+        instance.getImage().set(2, new Integer(100));
+        instance.getImage().set(3, new Integer(150));
+        instance.getImage().set(4, new Integer(100));
+      
         
-        assertEquals(expResult, result.getImage());
-        assertEquals(2, result.getHauteur());
-        assertEquals(3, result.getLongueur());
+        ImagePGM instance2 = new ImagePGM(4,4);
+        instance2.getImage().set(0, new Integer(0));
+        instance2.getImage().set(1, new Integer(0));
+        instance2.getImage().set(2, new Integer(50));
+        instance2.getImage().set(3, new Integer(200));
+        instance2.getImage().set(4, new Integer(100));
         
-        assertEquals(null, testImage.difference(new ImagePGM(3,3)));
+        
+        ImagePGM instance3 = new ImagePGM(4,4);
+        instance3.getImage().set(0, new Integer(0));
+        instance3.getImage().set(1, new Integer(255));
+        instance3.getImage().set(2, new Integer(50));
+        instance3.getImage().set(3, new Integer(0));
+        instance3.getImage().set(4, new Integer(0));
+        
+        ImagePGM instance4 = new ImagePGM(5,5);
+        ImagePGM instance5 = new ImagePGM(4,4,255);
+        
+        assertEquals(instance.difference(instance2).getImage(), instance3.getImage());
+        assertEquals(instance.difference(instance4).getImage(),instance5.getImage());
+        
+        
+        
     }
 }
